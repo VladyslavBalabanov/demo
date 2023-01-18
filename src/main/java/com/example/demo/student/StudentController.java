ddd0704072a663b7f7e02550529d863126cf2c3d
Vlad <vladyslav.balabanov@gmail.com>
2023-01-18 20:18:05 +0100

    Simplified imports and added README.md.

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,6 +1,5 @@
 package com.example.demo.student;
 
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
